f0937e2cd104cbf146739fa7195d7262db9c9ee0
fakerincs <142262169+fakerincs@users.noreply.github.com>
2024-01-19 19:23:01 +0000

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>test</p>
+        <a>learn react</a>
       </header>
     </div>
   );
